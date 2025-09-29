57d1beacca7b0ba03803b33453a47969b40abd58
Tripathi <eshaan.tripathi@innovasolutions.com>
2025-09-29 16:13:15 +0530

    3 commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Demo Service is live!" }),
+    body: JSON.stringify({ message: "Demo Service is running!" }),
   };
 };
