b553205d1adad10bf810ff55ea7d6fb90d870dc6
7chaitanya <117384464+7chaitanya@users.noreply.github.com>
2025-09-30 16:13:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Demo Service is running" }),
+    body: JSON.stringify({ message: " Service is running" }),
   };
 };
-/*hi*/
+/*hi  how are you  guys*/
