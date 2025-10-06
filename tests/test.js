765ca6ae0a965d95b9d46b25902b80a0df91a1b4
Tripathi <eshaan.tripathi@innovasolutions.com>
2025-10-06 11:42:35 +0530

    ih

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,6 @@ const { handler } = require('../src/index');
 
 test('should return success message', async () => {
   const response = await handler({});
-  expect(response.statusCode).toBe(404);
+  expect(response.statusCode).toBe(200);
   expect(JSON.parse(response.body).message).toBe("Demo Service is live!");
 });
