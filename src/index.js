ebe0e5f645928542ea3bde0d43f809b68df59848
Tripathi <eshaan.tripathi@innovasolutions.com>
2025-10-06 15:17:28 +0530

    jo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ exports.handler = async (event) => {
     console.log("Event:", event);
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Lambda executed successfully!" }),
+        body: JSON.stringify({ message: "Lambda executed successfully" }),
     };
 };
